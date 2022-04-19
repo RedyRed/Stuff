f173034d380a564833c173a8ed2725c0e130e82b
RVZ <100237543+RedyRed@users.noreply.github.com>
2022-04-19 18:38:37 +0300

    Update VenyxUILibEdited.lua

--- VenyxUILibEdited.lua
+++ VenyxUILibEdited.lua
@@ -302,7 +302,7 @@ do
 		}, library)
 	end
 	
-	function page.new(library, title, icon)
+	function page.new(library, title, icon, iconstr)
 		local button = utility:Create("TextButton", {
 			Name = title,
 			Parent = library.pagesContainer,
@@ -336,7 +336,7 @@ do
 				Position = UDim2.new(0, 12, 0.5, 0),
 				Size = UDim2.new(0, 16, 0, 16),
 				ZIndex = 3,
-				Image = icon
+				Image = iconstr
 				ImageColor3 = themes.TextColor,
 				ImageTransparency = 0.64,
 				ScaleType = Enum.ScaleType.Fit
